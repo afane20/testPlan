295f32260a44c9c1157850c298228f3476d8d76f
afane20 <salvadormcintosh@hotmail.com>
2015-03-08 05:55:08 -0600

    Testing

--- src/main/java/JSPDiscussion/LoginControl.java
+++ src/main/java/JSPDiscussion/LoginControl.java
@@ -84,10 +84,7 @@ public class LoginControl extends HttpServlet {
                 } else {
                 response.sendRedirect("InvalidLogin.jsp");
                 }
-              } else {
-               response.sendRedirect("InvalidLogin.jsp");
-                }
-                
+              } 
             }
             
            
