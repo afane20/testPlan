5fc055a17cae560894ce4f424ae36cedb1053393
afane20 <salvadormcintosh@hotmail.com>
2015-03-30 10:31:18 -0600

    Testing

--- src/main/java/JSPDiscussion/Login.java
+++ src/main/java/JSPDiscussion/Login.java
@@ -9,8 +9,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -32,7 +36,73 @@ public class Login extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {        
-        
+             // JDBC driver name and database URL
+   String JDBC_DRIVER = "com.mysql.jdbc.Driver";  
+   String DB_URL = "jdbc:mysql://localhost:3306/";
+   String dbName = "planit";
+   //  Database credentials
+   String USER = "root";
+   String PASS = "root";
+   
+   Connection conn = null;
+   Statement stmt = null;
+   try{
+      //STEP 2: Register JDBC driver
+      Class.forName("com.mysql.jdbc.Driver");
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL+dbName,USER,PASS);
+
+      //STEP 4: Execute a query
+      System.out.println("Creating statement...");
+      stmt = conn.createStatement();
+      String sql;
+      sql = "SELECT title, location FROM byui";
+      ResultSet rs = stmt.executeQuery(sql);
+
+      //STEP 5: Extract data from result set
+      while(rs.next()){
+         //Retrieve by column name
+         String title  = rs.getString("title");
+         //int age = rs.getInt("age");
+         String first = rs.getString("location");
+         //String last = rs.getString("last");
+
+         //Display values
+         System.out.print("ID: " + title + "\n");
+        // System.out.print(", Age: " + age);
+         System.out.print(", Name: " + first + "\n");
+//         System.out.println(", Last: " + last);
+      }
+      //STEP 6: Clean-up environment
+      rs.close();
+      stmt.close();
+      conn.close();
+   }catch(SQLException se){
+      //Handle errors for JDBC
+      se.printStackTrace();
+   }catch(Exception e){
+      //Handle errors for Class.forName
+      e.printStackTrace();
+   }finally{
+      //finally block used to close resources
+      try{
+         if(stmt!=null)
+            stmt.close();
+      }catch(SQLException se2){
+      }// nothing we can do
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+   }//end try
+   System.out.println("Goodbye!");
+//end of Query
+   
+            
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         
